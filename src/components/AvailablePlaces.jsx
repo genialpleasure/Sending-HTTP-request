0acd9ce671698ab1ea15a9d74a599cfd678db076
genialpleasure <isaacnpleasure01@gmail.com>
2025-12-22 23:59:47 +0100

    using fetch method

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const[availablePlaces, setAvailablePlaces] = useState([]);
+
+  fetch('http://localhost:5173/places')
+  .then((response)=>{
+    return response.json();
+  })
+  .then((resData)=> {
+    setAvailablePlaces(resData.places);
+  });
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
