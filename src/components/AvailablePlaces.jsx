00da9c11ae7e6c46b23f1200d08993d7c9ed16be
genialpleasure <isaacnpleasure01@gmail.com>
2026-02-17 23:57:41 +0100

    Haandling HTTP errors

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,20 +1,36 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const[isFetching, setIsFetching] = useState(false);
   const[availablePlaces, setAvailablePlaces] = useState([]);
+  const[error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
+
+      try {
+        const response = await fetch('http://localhost:3000/places');
       const resData = await response.json();
+
+      if (!response.ok){
+        throw new Error("Failed to fetch places data.");
+      }
       setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({message: error.message || "Something went wrong!"});
+      }
+      
       setIsFetching(false);
     }
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="Error" message={error.message} />;
+  }
 
   return (
     <Places
